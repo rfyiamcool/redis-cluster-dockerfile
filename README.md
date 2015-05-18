b028cb964dbcdf28d3874b551b5e4cdea6cc481b
ruifengyun <rfyiamcool@163.com>
2015-05-18 12:10:42 +0800

    support ip config

--- README.md
+++ README.md
@@ -4,9 +4,15 @@
 
 通过Dockerfile创建redis cluster的image，然后通过创建的镜像批量的生成容器...
 
+可以传递指定IP和端口
+
 ```
 docker build -t redis_cluster --rm .
 
 docker run -it --net=host redis_cluster 9000
+
+OR
+
+docker run -it --net=host redis_cluster 9000 10.10.32.88
 
 ```
--- manage.sh
+++ manage.sh
@@ -3,6 +3,10 @@ cd /app/redis
 redis_port=$1
 echo $redis_port
 cd /app/redis
+if [ -n $2 ];then
+    ip=$2 
+    sed -i "s/bind 127.0.0.1/bind $ip/g" redis.conf
+fi
 sed -i "s/6379/$redis_port/g" redis.conf
 sed -i 's/# cluster-enabled yes/cluster-enabled yes/g' redis.conf
 sed -i 's/# cluster-node-timeout 15000/cluster-node-timeout 15000/g' redis.conf
